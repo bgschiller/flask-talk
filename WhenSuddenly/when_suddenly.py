c2ebb8091df679ee41feb5aa2efa985a9126adc9
Brian Schiller <bgschiller@gmail.com>
2015-04-02 20:15:52 -0500

    Stub out the story view

--- WhenSuddenly/when_suddenly.py
+++ WhenSuddenly/when_suddenly.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/story/<room_name>')
+def story(room_name):
+    """Return the story for a given room"""
+    # This story taken from http://foldingstory.com/pfuf2/
+    return '<br>'.join([
+      "Nobody knows how Saturn got its rings. But I have a theory.",
+      "A theory that has been ignored by pretty much everyone at this Cosmic Conference. It's really because my theory is so awesome that all these so-called academics are jealous. Saturn",
+      "should not be a planet anymore. We got rid of Pluto and no one misses it, so Saturn should be next, because real planets don't have rings. The Cosmic Conference started throwing",
+      "planets out left &amp; right. Mercury was too small. Neptune? Who cares? Pretty soon it was just Venus, Earth, Mars, and Jupiter. Oh, and the sun. Who could forget good old Planet Sun.",
+      "After creationism became the law of the land, they issued a decree that the Sun did indeed revolve around the Earth. Elsewhere, pagans decided to bring back",
+      "Worship of the stag god, and to that end they first had to rescue the Gundestrup Cauldron from the heathens who had taken over the senate, and who were using it to",
+      "promote the cult of Cernunnos. Even Torquatus showed them favor. The Republic was in danger. The cult could not persist without the rites..The Gundestrup Cauldron must be taken!",
+      "I hastily drafted some legionnaires at a local taverna, and stormed the villa where the cult held its mysterious cauldron. Some of my men fell at the gate out of sheer befuddlement",
+      "but the rest of us closed ranks and persisted until we found the villa's inner sanctum. The residents round their cauldron looked at us from their bizarre masks. Our legionnaires f",
+      "formed, once again, the ancient circle and spoke the mystic oath. The aftermath, indescribable. At last our mission was complete. Silently, we exited, the treasure was ours."
+    ])
+
 
 if __name__ == '__main__':
     app.run()
