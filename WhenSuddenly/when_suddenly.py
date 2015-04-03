e2909a3fb9846fb3b8b4aa2d0c9120682632f065
Brian Schiller <bgschiller@gmail.com>
2015-04-03 11:59:45 -0500

    Add textarea form

--- WhenSuddenly/when_suddenly.py
+++ WhenSuddenly/when_suddenly.py
@@ -14,7 +14,7 @@ def story(room_name):
         {"author": "lucielucie", "text": "Nobody knows how Saturn got its rings. But I have a theory."},
         {"author": "MoralEnd", "text": "A theory that has been ignored by pretty much everyone at this Cosmic Conference. It's really because my theory is so awesome that all these so-called academics are jealous. Saturn"},
         {"author": "MangoMania", "text": "should not be a planet anymore. We got rid of Pluto and no one misses it, so Saturn should be next, because real planets don't have rings. The Cosmic Conference started throwing"},
-        {"author": "49erFaithful", "text": "planets out left &amp; right. Mercury was too small. Neptune? Who cares? Pretty soon it was just Venus, Earth, Mars, and Jupiter. Oh, and the sun. Who could forget good old Planet Sun."},
+        {"author": "49erFaithful", "text": "planets out left & right. Mercury was too small. Neptune? Who cares? Pretty soon it was just Venus, Earth, Mars, and Jupiter. Oh, and the sun. Who could forget good old Planet Sun."},
         {"author": "Chaz", "text": "After creationism became the law of the land, they issued a decree that the Sun did indeed revolve around the Earth. Elsewhere, pagans decided to bring back"},
         {"author": "Benwicky", "text": "Worship of the stag god, and to that end they first had to rescue the Gundestrup Cauldron from the heathens who had taken over the senate, and who were using it to"},
         {"author": "SlimWhitman", "text": "promote the cult of Cernunnos. Even Torquatus showed them favor. The Republic was in danger. The cult could not persist without the rites..The Gundestrup Cauldron must be taken!"},
